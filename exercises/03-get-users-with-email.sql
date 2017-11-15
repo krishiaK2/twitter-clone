fd96a7ba0ecf532169775b608f19efccafb3c907
krishiaK2 <2911.samson@gmail.com>
2017-11-15 16:13:24 +0800

    Update 03-get-users-with-email.sql

--- exercises/03-get-users-with-email.sql
+++ exercises/03-get-users-with-email.sql
@@ -13,10 +13,4 @@
 --
 -- Write your query below:
 
-CREATE TABLE IF NOT EXISTS users (
-	fullname VARCHAR NOT NULL,
-	username VARCHAR(15) UNIQUE NOT NULL,
-	email  VARCHAR PRIMARY KEY,
-	password VARCHAR NOT NULL,
-	created_at TIMESTAMP DEFAULT now()
-);
+SELECT * FROM users WHERE email = '{{email}}';
