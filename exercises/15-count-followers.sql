b000baddcb57cdb745804060bba459b966f7d8cb
krishiaK2 <2911.samson@gmail.com>
2017-12-19 18:56:12 +0800

    Add files via upload

---
+++ exercises/15-count-followers.sql
@@ -0,0 +1,9 @@
+-- Count how many users are following the given user.
+--
+-- You have access to the following variables which can be used as placeholders
+-- for actual values:
+--     - email
+--
+-- Write your query below:
+
+SELECT COUNT(followers) FROM follows WHERE following = '{{email}}'
